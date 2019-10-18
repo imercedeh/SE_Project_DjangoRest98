7d1f1fd02bcdcf71d5f61d48b75911c61817ed43
Mercedeh <Imercedeh@gmail.com>
2019-10-18 17:43:33 +0330

    Add user model

--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class user(User):
+    itinerary=models.CharField(max_length=500)
+    is_leader=models.BooleanField(default=False)
+    phone_number=models.CharField(max_length=13)
