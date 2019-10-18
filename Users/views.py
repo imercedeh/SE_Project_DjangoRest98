86499213f4ae45358def08843f4a27c91a39911b
Mercedeh <Imercedeh@gmail.com>
2019-10-18 18:14:42 +0330

    Add user info view

--- Users/views.py
+++ Users/views.py
@@ -1,2 +1,16 @@
 from django.shortcuts import render
-# Create your views here.
+from .models import *
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserSerializer
+from rest_framework import status
+
+class UserAPI(APIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UserSerializer
+
+    def get(self, request, format=None):
+        u=user.objects.get(username=request.user.username)
+        serializer=self.serializer_class(u)
+        return Response(serializer.data,status=status.HTTP_200_OK)
