b0d8d12e9738441b52012cf02bc854348a065248
Mercedeh <Imercedeh@gmail.com>
2019-10-18 20:04:57 +0330

    Add leader profile url

--- Users/urls.py
+++ Users/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from rest_framework.authtoken import views
-from Users.views import SignupAPI,UserAPI,Become_LeaderAPI
+from Users.views import SignupAPI,UserAPI,Become_LeaderAPI,LeaderAPI
 
 from rest_framework import routers
 from rest_framework.authtoken import views
@@ -15,4 +15,5 @@ urlpatterns = [
     url(r'^token/refresh/$', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     url(r'^me/$', UserAPI.as_view()),
     url(r'^become-leader/$', Become_LeaderAPI.as_view()),
+    url(r'^me/leader/$', LeaderAPI.as_view()),
 ]
