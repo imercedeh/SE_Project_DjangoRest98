6a3f0e05a629a51a7c82a667fbbac86ba12cb66b
Mercedeh <Imercedeh@gmail.com>
2019-10-18 19:51:55 +0330

    Add become leader url

--- Users/urls.py
+++ Users/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import include, url
 from rest_framework.authtoken import views
-from Users.views import SignupAPI
-from Users.views import UserAPI
-
+from Users.views import SignupAPI,UserAPI,Become_LeaderAPI
 
 from rest_framework import routers
 from rest_framework.authtoken import views
@@ -16,4 +14,5 @@ urlpatterns = [
     url(r'^token/$', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     url(r'^token/refresh/$', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     url(r'^me/$', UserAPI.as_view()),
+    url(r'^become-leader/$', Become_LeaderAPI.as_view()),
 ]
