a896b85de5ff71b13d55b8b5557886d1f7276330
Mercedeh <Imercedeh@gmail.com>
2019-10-18 19:14:14 +0330

    Add leader model

--- Users/models.py
+++ Users/models.py
@@ -5,3 +5,10 @@ class user(User):
     itinerary=models.CharField(max_length=500)
     is_leader=models.BooleanField(default=False)
     phone_number=models.CharField(max_length=13)
+
+class Leader(models.Model):
+    userID=models.OneToOneField(user,on_delete=models.CASCADE)
+    nationalID=models.IntegerField()
+    has_car=models.BooleanField(default=False)
+    car_capacity=models.IntegerField()
+    car_model=models.CharField(max_length=20)
