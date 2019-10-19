2ecf82d2b984ee3d3ac013546e93c41f58cf3f99
Mobina Kashanian <Mobina.km@gmail.com>
2019-10-19 14:31:14 +0330

    Add a Primitive Place Model.

--- Places/models.py
+++ Places/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Places(models.Model):
+    Title=models.CharField(max_length=500)
+    Image=models.ImageField(upload_to="E:\SE_Project_DjangoRest98/photos",null=True,blank=False)
+    Description=models.TextField()
+    
+    def __unicode__(self):
+        return self.Title
+
+    def __str__(self):
+        return self.Title
