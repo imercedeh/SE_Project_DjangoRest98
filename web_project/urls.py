0e0b512cdbc60dc668460bf074df3172c3b700b7
Mobina Kashanian <Mobina.km@gmail.com>
2019-10-23 15:07:50 +0330

    Add PlacesApp urls in web_project.

--- web_project/urls.py
+++ web_project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls import include, url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('Users.urls')),
+    url(r'api/Places/', include("Places.api.urls")),
 ]
