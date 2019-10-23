69436a4596b84b8c5eabb79728fde79c29e953b0
Mobina Kashanian <Mobina.km@gmail.com>
2019-10-24 00:15:44 +0330

    Edit CreatePlace Serializer With Enhanced Model.

--- Places/api/serializers.py
+++ Places/api/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework.serializers import *
 from Places.models import Places,PlaceImage
+from rest_framework import serializers
+from django.db.models import Avg
 
 class PlaceImageSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,10 +10,41 @@ class PlaceImageSerializer(serializers.ModelSerializer):
 
 
 class CreatePlaceSerializer(ModelSerializer):
+    images = PlaceImageSerializer(source='taskimage_set', many=True)
+    Average1=Places.objects.all().aggregate(Avg('Likes'))
     class Meta:
         model=Places
-        fields=[
-            'Title',
-            'Image',
-            'Description'
+        fields = [
+        'id',
+        'title', 
+        'user',
+        'images',
+        'Description',
+        'Likes',
+        'categories',
+        'Hardness',
+        'Address',
+        'Time',
+        'City',
+        'Average',
         ]
+        read_only_fields = ('Average',)
+
+
+    def create(self, validated_data):
+        images_data = self.context.get('image').request.FILES
+        places = Places.objects.create(
+        title=validated_data.get('title', 'no-title'),
+        Description=validated_data.get('Description'),
+        Likes=validated_data.get('Likes'),
+        categories=validated_data.get('categories'),
+        Hardness=validated_data.get('Hardness'),
+        Address=validated_data.get('Address'),
+        Time=validated_data.get('Time'),
+        City=validated_data.get('City'),
+        Average=Places.objects.all().aggregate(Avg('Likes'))
+        ,user_id=1)
+
+        for image_data in images_data.values():
+            PlaceImage.objects.create(places=places, image=image_data)
+        return places
