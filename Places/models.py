69b9fcf6ec96688a8ad366d22d01c4af0ab22843
Mobina Kashanian <Mobina.km@gmail.com>
2019-10-23 23:47:03 +0330

    Add Enhanced PlaceModel.

--- Places/models.py
+++ Places/models.py
@@ -3,12 +3,29 @@ from django.db import models
 # Create your models here.
 
 class Places(models.Model):
-    Title=models.CharField(max_length=500)
-    Image=models.ImageField(upload_to="E:\SE_Project_DjangoRest98/photos",null=True,blank=False)
-    Description=models.TextField()
+    CATEGORY = (
+        ("Historical", "Historical"),
+        ("museums", "museums"),
+        ("Forests", "Forests"),
+        ("Public art","Public art"),
+        ("national parks","national parks"),
+    )
+    title = models.CharField(max_length=102, blank=False,default='بدون عنوان')
+    Description=models.TextField(default='بدون توضیحات', blank=False)
+    Likes = models.CharField(max_length=1, blank=False, null=True,default='0')
+    categories=models.CharField(max_length=200,blank=False,null=False,choices=CATEGORY,default=CATEGORY[1][1])
+    Hardness=models.CharField(max_length=102, blank=False,default='بدون درجه ی سختی')
+    Address=models.CharField(max_length=102, blank=False,default='بدون آدرس ')
+    Time=models.CharField(max_length=102, blank=False,default='بدون  تخمین زمان ')
+    City=models.CharField(max_length=102, blank=False,default='بدون شهر')
+    Average=models.CharField(max_length=102, blank=False,default='0')
     
     def __unicode__(self):
-        return self.Title
+        return self.title
 
     def __str__(self):
-        return self.Title
+        return self.title
+
+class PlaceImage(models.Model): 
+    places = models.ManyToManyField(Places)
+    image = models.FileField(blank=True)
