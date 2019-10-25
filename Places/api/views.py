53b47c0d4f065cf97980be23f35915a8fd745b5d
Mobina Kashanian <Mobina.km@gmail.com>
2019-10-25 13:33:14 +0330

    Add ViewPlace APIView.

--- Places/api/views.py
+++ Places/api/views.py
@@ -1,13 +1,32 @@
 from django.db.models import Q
 from rest_framework.generics import CreateAPIView
-from .serializers import CreatePlaceSerializer
 from Places.models import Places
-from .serializers import CreatePlaceSerializer
+from .serializers import CreatePlaceSerializer,ViewPlaceSerializer
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework import generics
+from django.http import Http404
+from rest_framework.filters import (
+        SearchFilter,
+        OrderingFilter,
+)
 
 class CreatePlaceAPIView(CreateAPIView):
     queryset = Places.objects.all()
     serializer_class=CreatePlaceSerializer
 
 
+class ViewPlaceAPI(generics.ListAPIView):
+    queryset = Places.objects.all()
+    serializer_class=ViewPlaceSerializer
+    filter_backends= [SearchFilter, OrderingFilter]
+    search_fields = ['City', 'title']
+
+    # def get_queryset(self, *args, **kwargs):
+    #     queryset_list = Places.objects.all() 
+        # user = self.request.user
+
+        # if user is not None:
+        #     return Places.objects.filter(user=user)
+        # else:
+        #     return Http404
