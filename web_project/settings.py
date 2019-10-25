ec466807474b700b4ceba83889f37a4374680c4a
Mobina Kashanian <Mobina.km@gmail.com>
2019-10-25 13:34:24 +0330

    Set FilterBackends in Settings.

--- web_project/settings.py
+++ web_project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
