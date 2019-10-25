5abc7ce5853033aa2ca032316b103df875e7112d
Mobina Kashanian <Mobina.km@gmail.com>
2019-10-25 13:35:57 +0330

    Add ViewPlace Url.

--- Places/api/urls.py
+++ Places/api/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from .views import CreatePlaceAPIView
+from .views import CreatePlaceAPIView,ViewPlaceAPI
 
 urlpatterns = [
-    url(r'CreatePlace/', CreatePlaceAPIView.as_view(), name='create'),   
+    url(r'CreatePlace/', CreatePlaceAPIView.as_view(), name='create'), 
+    url(r'ViewPlace/', ViewPlaceAPI.as_view(), name='View'),   
 ]
