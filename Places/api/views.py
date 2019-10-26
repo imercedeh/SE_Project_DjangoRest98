ee27f48094017e5d63a14d121c30db2980ea0884
Mobina Kashanian <Mobina.km@gmail.com>
2019-10-26 20:51:38 +0330

    Edit CreatePlace Serializer.

--- Places/api/views.py
+++ Places/api/views.py
@@ -2,12 +2,13 @@ from django.db.models import Q
 from rest_framework.generics import CreateAPIView
 from .serializers import CreatePlaceSerializer
 from Places.models import Places
-from .serializers import CreatePlaceSerializer
+from .serializers import CreatePlaceSerializer,PlaceImageSerializer
 from rest_framework import status
 from rest_framework.response import Response
 
 class CreatePlaceAPIView(CreateAPIView):
     queryset = Places.objects.all()
     serializer_class=CreatePlaceSerializer
+    serializer_class1=PlaceImageSerializer
 
 
