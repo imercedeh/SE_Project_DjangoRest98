35d7efa87265c58ac5dfe1f686629701620eeba8
Mobina Kashanian <Mobina.km@gmail.com>
2019-10-27 23:07:28 +0330

    Edit CreatePlace Serializer.

--- Places/api/serializers.py
+++ Places/api/serializers.py
@@ -4,13 +4,14 @@ from rest_framework import serializers
 from django.db.models import Avg
 
 class CreatePlaceSerializer(ModelSerializer):
-    images = PlaceImageSerializer(source='placeimage_set', many=True)
     class Meta:
         model=Places
         fields = [
         'id',
         'title', 
-        'images',
+        'image1',
+        'image2',
+        'image3',
         'Description',
         'Likes',
         'categories',
@@ -25,27 +26,6 @@ class CreatePlaceSerializer(ModelSerializer):
         read_only_fields = ('Average',)
 
 
-
-    def create(self, validated_data):
-        images_data = self.context.get('view').request.FILES
-        places = Places.objects.create(
-        title=validated_data.get('title', 'no-title'),
-        Description=validated_data.get('Description'),
-        Likes=validated_data.get('Likes'),
-        categories=validated_data.get('categories'),
-        Hardness=validated_data.get('Hardness'),
-        Address=validated_data.get('Address'),
-        Time=validated_data.get('Time'),
-        StartTime=validated_data.get('StartTime'),
-        EndTime=validated_data.get('EndTime'),
-        City=validated_data.get('City'), 
-        Average=Places.objects.all().aggregate(Avg('Likes'))
-        ,)
-
-        for image_data in images_data.values():
-            PlaceImage.objects.create(places=places, image=image_data)
-        
-        return places
 
 class ViewPlaceSerializer(ModelSerializer):
     class Meta:
