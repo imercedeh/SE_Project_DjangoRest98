3e70418480ade18d90272b20f6d1f4bfa43fa5d5
Mobina Kashanian <Mobina.km@gmail.com>
2019-10-27 23:30:57 +0330

    Set Media UrlPatterns in Urls of MainProject.

--- web_project/urls.py
+++ web_project/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls import include, url
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('Users.urls')),
     url(r'api/Places/', include("Places.api.urls")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
