4a6904b94ea0c9f747dceb080cf347247b8c0018
Mercedeh <Imercedeh@gmail.com>
2019-11-09 00:26:54 +0330

    Add leadplace url

--- Users/urls.py
+++ Users/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from rest_framework.authtoken import views
-from Users.views import SignupAPI,LeaderCreationAPI,ProfileAPI,SpecificLeaderAPI,SpecificUserAPI
+from Users.views import SignupAPI,LeaderCreationAPI,ProfileAPI,SpecificLeaderAPI,SpecificUserAPI,LeadPlaceAPI
 
 from rest_framework import routers
 from rest_framework.authtoken import views
@@ -17,5 +17,6 @@ urlpatterns = [
     url(r'^leadercreation/$', LeaderCreationAPI.as_view()),
     url(r'^me/$', ProfileAPI.as_view()),
     url(r'SpecificLeader/',SpecificLeaderAPI.as_view()),
-    url(r'SpecificUser/',SpecificUserAPI.as_view())
+    url(r'SpecificUser/',SpecificUserAPI.as_view()),
+    url(r'LeadPlace/',LeadPlaceAPI.as_view()),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
