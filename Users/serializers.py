7190c54d31738e6057e85a9b545d0734bee38e8b
Mercedeh <Imercedeh@gmail.com>
2019-11-09 22:11:45 +0330

    Add signup serializer

--- Users/serializers.py
+++ Users/serializers.py
@@ -1,6 +1,15 @@
 from django.contrib.auth import authenticate
 from .models import user,Leader
 from rest_framework import serializers
+
+class SignupSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password=serializers.CharField()
+    email = serializers.EmailField(allow_blank=True)
+    first_name = serializers.CharField(allow_blank=True)
+    last_name = serializers.CharField(allow_blank=True)
+    itinerary=serializers.CharField(max_length=500,allow_blank=True)
+    phone_number=serializers.CharField(max_length=13,allow_blank=True)
     
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
