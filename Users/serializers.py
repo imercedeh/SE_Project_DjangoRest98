bfa0ae9284ad72c9550a8a1e29ddc2adabc517b0
Mercedeh <Imercedeh@gmail.com>
2019-11-09 04:17:15 +0330

    Fix a bug

--- Users/serializers.py
+++ Users/serializers.py
@@ -13,7 +13,7 @@ class LeaderCreationSerializer(serializers.Serializer):
     car_capacity=serializers.CharField(allow_blank=True)
     car_model=serializers.CharField(allow_blank=True)
     gender=serializers.BooleanField()
-    age=serializers.CharField()
+    age=serializers.IntegerField()
 
 class LeaderSerializer(serializers.ModelSerializer):
     class Meta:
