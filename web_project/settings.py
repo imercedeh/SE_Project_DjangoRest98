eb78f935419946b40d43648c78a867ebf02cc3f8
Mobina <mobina.km@gmail.com>
2019-11-25 12:46:48 +0330

    Add TravelLouge app in settings.

--- web_project/settings.py
+++ web_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'TravelLouge',
 ]
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
