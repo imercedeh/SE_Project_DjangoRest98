df6838b8616705d82fe6c9175a62cbc6a4b23234
Mobina <mobina.km@gmail.com>
2019-11-25 12:49:00 +0330

    Add TravelLouge Model.

--- TravelLouge/models.py
+++ TravelLouge/models.py
@@ -4,11 +4,11 @@ from Users.models import Leader,User
 
 # Create your models here.
 class TravelLouge(models.Model):
-    title = models.CharField(max_length=102, blank=False,default='بدون عنوان')
-    description=models.TextField(default='بدون توضیحات', blank=False)
+    title = models.CharField(max_length=102, blank=False,default='بدون موضوع')
+    description=models.TextField(default='بدون توضیح', blank=False)
     image1=models.ImageField(upload_to='image/%Y/%m/%d/',blank=True)
     image2=models.ImageField(upload_to='image/%Y/%m/%d/',blank=True)
     image3=models.ImageField(upload_to='image/%Y/%m/%d/',blank=True)
     leader=models.ForeignKey(Leader,on_delete=models.CASCADE)
     user=models.ForeignKey(User,on_delete=models.CASCADE)
-    places=models.ForeignKey(Places,on_delete=models.CASCADE)
+    places=models.ForeignKey(Places,on_delete=models.CASCADE)  
