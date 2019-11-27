6d90922dbdc9e861a0fd8a7cea48b99f143c8b11
Mercedeh <Imercedeh@gmail.com>
2019-11-27 22:16:30 +0330

    Add a url

--- TravelLouge/urls.py
+++ TravelLouge/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from rest_framework.authtoken import views
-from TravelLouge.views import TravelLougeCreationAPI
+from TravelLouge.views import TravelLougeCreationAPI,PlaceTravelLougesAPI
 
 from rest_framework import routers
 from rest_framework.authtoken import views
@@ -11,4 +11,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^travellouge-creation/$', TravelLougeCreationAPI.as_view()),
+    url(r'^place-travellouges/$', PlaceTravelLougesAPI.as_view()),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
