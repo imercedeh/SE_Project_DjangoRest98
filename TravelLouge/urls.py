448742a11ed73c082139491d5bf8d365aae3f03e
Mercedeh <Imercedeh@gmail.com>
2019-11-28 00:34:31 +0330

    Add a url for specific travellouge

--- TravelLouge/urls.py
+++ TravelLouge/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from rest_framework.authtoken import views
-from TravelLouge.views import TravelLougeCreationAPI,PlaceTravelLougesAPI
+from TravelLouge.views import TravelLougeCreationAPI,PlaceTravelLougesAPI,SpecificTravellougeAPI
 
 from rest_framework import routers
 from rest_framework.authtoken import views
@@ -12,4 +12,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^travellouge-creation/$', TravelLougeCreationAPI.as_view()),
     url(r'^place-travellouges/$', PlaceTravelLougesAPI.as_view()),
+    url(r'^SpecificTravellouge/$', SpecificTravellougeAPI.as_view()),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
