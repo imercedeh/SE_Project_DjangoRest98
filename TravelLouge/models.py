3a817f3b252c22cdc0e84b1dd13d3ee9a1d6d4ac
Mercedeh <Imercedeh@gmail.com>
2019-11-27 15:31:11 +0330

    Edit model and makemigrations

--- TravelLouge/models.py
+++ TravelLouge/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 from Places.models import Places
-from Users.models import Leader,User
+from Users.models import Leader,user
 
 # Create your models here.
 class TravelLouge(models.Model):
-    title = models.CharField(max_length=102, blank=False,default='بدون موضوع')
-    description=models.TextField(default='بدون توضیح', blank=False)
-    image1=models.ImageField(upload_to='image/%Y/%m/%d/',blank=True)
-    image2=models.ImageField(upload_to='image/%Y/%m/%d/',blank=True)
-    image3=models.ImageField(upload_to='image/%Y/%m/%d/',blank=True)
-    leader=models.ForeignKey(Leader,on_delete=models.CASCADE)
-    user=models.ForeignKey(User,on_delete=models.CASCADE)
-    places=models.ForeignKey(Places,on_delete=models.CASCADE)  
+    auther=models.ForeignKey(user,on_delete=models.CASCADE)
+    title = models.CharField(max_length=102)
+    description=models.CharField(max_length=1000)
+    image1=models.ImageField(upload_to='image/%Y/%m/%d/',default='travellouge/defualt.jpg')
+    image2=models.ImageField(upload_to='image/%Y/%m/%d/',default='travellouge/defualt.jpg')
+    image3=models.ImageField(upload_to='image/%Y/%m/%d/',default='travellouge/defualt.jpg')
+    places=models.ManyToManyField(Places)  
