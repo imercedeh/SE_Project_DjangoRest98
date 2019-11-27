b23ffceea65d9c67d748a9d15fbc9dd97bf121f9
Mercedeh <Imercedeh@gmail.com>
2019-11-27 18:17:32 +0330

    Add travellouge serializer

--- TravelLouge/serializers.py
+++ TravelLouge/serializers.py
@@ -7,3 +7,8 @@ class TravelLougeCreationSerializer(serializers.Serializer):
     title = serializers.CharField(allow_blank=True,max_length=102)
     description = serializers.CharField(allow_blank=True,max_length=1000)
     places=serializers.ListField(child=serializers.IntegerField(),allow_empty=True)
+
+class TravellougeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TravelLouge
+        fields = '__all__'
