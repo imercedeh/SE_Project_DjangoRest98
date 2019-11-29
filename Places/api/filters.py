64b2b0337d8f5683e7e7a00bedd42100eaf28107
Mobina <mobina.km@gmail.com>
2019-11-29 18:30:17 +0330

    Edit filtery.py

--- Places/api/filters.py
+++ Places/api/filters.py
@@ -1,8 +1,11 @@
 import django_filters
 from django_filters import rest_framework as filters
-
 from Places.models import Places
+from rest_framework import filters
 
+class DynamicSearchFilter(filters.SearchFilter):
+    def get_search_fields(self, view, request):
+        return request.GET.getlist('search_fields', [])
 
 class PlacesFilter(django_filters.FilterSet):
     class Meta:
