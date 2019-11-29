511ae01bca82eb62c2809c21e054433d6638002b
Mobina <mobina.km@gmail.com>
2019-11-29 21:03:30 +0330

    Add Test for filter.

--- Places/api/tests/test_views.py
+++ Places/api/tests/test_views.py
@@ -165,3 +165,92 @@ class TestView(APITestCase):
         response=self.client.get(url)
         self.assertEqual(response.status_code,200)
         self.assertEqual(qs.count(),1)
+
+
+        
+
+    def test_queryset_Advancefilter(self):
+        queryset=Places.objects.all()
+        qs=queryset.filter(StartTime=8).distinct()
+        self.assertEqual(len(qs),2)
+        url='http://127.0.0.1:8000/api/Places/PlaceAdvanceSearch/?id=&title=&Likes=&categories=&Hardness=&Time=&StartTime=8&EndTime=10&City=%D8%AA%D9%87%D8%B1%D8%A7%D9%86'
+        data= {
+            "id": 3,
+        "leader": [],
+        "title": "کاخ سعد آباد",
+        "Description": "جموعه فرهنگی تاریخی سعدآباد به مجموعه عمارت‌ها و کاخ‌هایی گفته می‌شود که در دربند، شمالی‌ترین و خوش آب و هواترین منطقه الوند در زمینی به مساحت ۱۱۰ هکتار[۱] بنا شده‌است.",
+        "Likes": "2",
+        "categories": "موزه",
+        "Hardness": "متوسط",
+        "Address": "نیاوران",
+        "Time": "2",
+        "StartTime": "8",
+        "EndTime": "10",
+        "City": "تهران",
+        "Average": "0",
+        "image1": "http://127.0.0.1:8000/media/image/2019/11/11/ef4e000e25d3a69277551ad565d573e4.jpg",
+        "image2": "http://127.0.0.1:8000/media/image/2019/11/11/download_1.jpg",
+        "image3": "http://127.0.0.1:8000/media/image/2019/11/11/download.jpg"
+        }
+        response=self.client.get(url)
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(qs.count(),2)
+
+
+
+    def test_queryset_Advancefilter1(self):
+        queryset=Places.objects.all()
+        qs=queryset.filter(StartTime=8).filter(EndTime=80).distinct()
+        self.assertEqual(len(qs),0)
+        url='http://127.0.0.1:8000/api/Places/PlaceAdvanceSearch/?id=&title=&Likes=&categories=&Hardness=&Time=&StartTime=8&EndTime=10&City=%D8%AA%D9%87%D8%B1%D8%A7%D9%86'
+        data= {
+            "id": 3,
+        "leader": [],
+        "title": "کاخ سعد آباد",
+        "Description": "جموعه فرهنگی تاریخی سعدآباد به مجموعه عمارت‌ها و کاخ‌هایی گفته می‌شود که در دربند، شمالی‌ترین و خوش آب و هواترین منطقه الوند در زمینی به مساحت ۱۱۰ هکتار[۱] بنا شده‌است.",
+        "Likes": "2",
+        "categories": "موزه",
+        "Hardness": "متوسط",
+        "Address": "نیاوران",
+        "Time": "2",
+        "StartTime": "8",
+        "EndTime": "10",
+        "City": "تهران",
+        "Average": "0",
+        "image1": "http://127.0.0.1:8000/media/image/2019/11/11/ef4e000e25d3a69277551ad565d573e4.jpg",
+        "image2": "http://127.0.0.1:8000/media/image/2019/11/11/download_1.jpg",
+        "image3": "http://127.0.0.1:8000/media/image/2019/11/11/download.jpg"
+        }
+        response=self.client.get(url)
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(qs.count(),0)
+
+
+    def test_filter2(self):
+        factory=RequestFactory()
+        base_url='http://127.0.0.1:8000/api/Places/PlaceAdvanceSearch/?id=&title=&Likes=&categories=&Hardness=&Time=&StartTime=8&EndTime=&City=سمنان'
+        request=factory.get(base_url)
+        data= {
+        "id": 6,
+        "leader": [],
+        "title": "کویر حاج علی قلی",
+        "Description": "کویر حاج علی قلی در استان سمنان واقع است. کویر حاج علی قلی با ۲۳۹۱ کیلومتر مربع مساحت , در جنوب شرقی شهر دامغان قرار دارد . این کویر از جنوب به کوه های دولت دیار , کوه خرس و کوه ترکمن گدر از جنوب غربی به کوه های کوه پنج و کوه سرخ , از غرب به دهستان فرات از شمال به کویر دامغان و از شرق به کوه اهوند محدود می شود .",
+        "Likes": "2",
+        "categories": "طبیعت",
+        "Hardness": "متوسط",
+        "Address": "جنوب شهر دامغان",
+        "Time": "2",
+        "StartTime": "8",
+        "EndTime": "10",
+        "City": "کابل",
+        "Average": "0",
+        "image1": "http://127.0.0.1:8000/media/image/2019/11/11/%DA%A9%D9%88%DB%8C%D8%B1-%D8%AD%D8%A7%D8%AC%D8%B9%D9%84%DB%8C-%D9%82%D9%84%DB%8C-768x460.jpg",
+        "image2": "null",
+        "image3": "null",
+        }
+        queryset=Places.objects.all()
+        qs=queryset.filter(City='سمنان').distinct()
+        response=self.client.get(base_url)
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(qs.count(),0)
+        # queryset=Places.objects.all()
