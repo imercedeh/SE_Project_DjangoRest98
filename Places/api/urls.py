e9db2c51c21af744879c0899c9924618955903cd
Mobina <mobina.km@gmail.com>
2019-11-29 18:32:34 +0330

    Add SearchView Url.

--- Places/api/urls.py
+++ Places/api/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from .views import CreatePlaceAPIView,ViewPlaceAPI,UniquePlaceAPI,RandomPlaces
+from .views import CreatePlaceAPIView,ViewPlaceAPI,UniquePlaceAPI,RandomPlaces,SearchView
 
 urlpatterns = [
     url(r'CreatePlace/', CreatePlaceAPIView.as_view(), name='create'), 
     url(r'ViewPlace/', ViewPlaceAPI.as_view(), name='View'), 
     url(r'UniquePlace/',UniquePlaceAPI.as_view(),name='unique'),  
-    url(r'RandomPlace/',RandomPlaces.as_view(),name='random')  
+    url(r'RandomPlace/',RandomPlaces.as_view(),name='random'),
+    url(r'SearchView/',SearchView.as_view(),name='serachview'),
 ]
