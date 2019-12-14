ad4d9f6e26e9cb644fd788fa7793df19af5a8e9e
Mercedeh <Imercedeh@gmail.com>
2019-12-14 21:41:39 +0330

    Add user service urls

--- UserService/UserService/urls.py
+++ UserService/UserService/urls.py
@@ -15,7 +15,31 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
+from rest_framework.authtoken import views
+from Users.views import SignupAPI,LeaderCreationAPI,ProfileAPI,SpecificLeaderAPI,SpecificUserAPI,LeadPlaceAPI,LeadersView,LeaderAdvanceSearch,LeaderSortView,UserAdvanceSearch,UsersView,ChangeAvailabilityAPI
+
+from rest_framework import routers
+from rest_framework.authtoken import views
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework_simplejwt import views as jwt_views
+#router = routers.DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    url(r'^sign-up/$', SignupAPI.as_view()),
+    url(r'^token/$', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    url(r'^token/refresh/$', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    url(r'^leadercreation/$', LeaderCreationAPI.as_view()),
+    url(r'^me/$', ProfileAPI.as_view()),
+    url(r'SpecificLeader/',SpecificLeaderAPI.as_view()),
+    url(r'SpecificUser/',SpecificUserAPI.as_view()),
+    url(r'LeadPlace/',LeadPlaceAPI.as_view()),
+    url(r'ChangeAvailability/',ChangeAvailabilityAPI.as_view()),
+    url('LeadersView/',LeadersView.as_view(),name='LeadersView'),
+    url('LeaderSortView/',LeaderSortView.as_view(),name='LeaderSortView'),
+    url('LeaderAdvanceSearch/',LeaderAdvanceSearch.as_view({'get': 'list'}),name='LeaderAdvanceSearch'),
+    url('UserAdvanceSearch/',UserAdvanceSearch.as_view({'get': 'list'}),name='UserAdvanceSearch'),
+    url('UsersView/',UsersView.as_view(),name='UsersView'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
