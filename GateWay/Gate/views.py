2d84fcc0caec57b186d83d27ff7322a3a866a05d
Mercedeh <Imercedeh@gmail.com>
2019-12-14 18:57:15 +0330

    Add sign-up view

--- GateWay/Gate/views.py
+++ GateWay/Gate/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
+import requests
+from rest_framework.views import APIView
 
-# Create your views here.
+UserServiceUrl="http://localhost:8001/api"
+
+class Signup(APIView):
+    permission_classes = (AllowAny,)
+    def post(self, request):
+        url=UserServiceUrl+"/sign-up/"
+        data=request.data
+        resualt=requests.post(url=url,data=data)
+        return Response(data=resualt.json())
+
+        
+
+
+        
