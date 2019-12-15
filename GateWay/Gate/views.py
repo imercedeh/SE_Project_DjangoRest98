ff5fe1c612a8c4ed7d69648867fdc204385725f3
Mercedeh <Imercedeh@gmail.com>
2019-12-15 13:28:10 +0330

    Edit user service urls in view

--- GateWay/Gate/views.py
+++ GateWay/Gate/views.py
@@ -5,12 +5,12 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 import requests
 from rest_framework.views import APIView
 
-UserServiceUrl="http://localhost:8001/api"
+UserServiceUrl="http://localhost:8001/api/User/"
 
 class Signup(APIView):
     permission_classes = (AllowAny,)
     def post(self, request):
-        url=UserServiceUrl+"/sign-up/"
+        url=UserServiceUrl+"sign-up/"
         data=request.data
         resualt=requests.post(url=url,data=data)
         return Response(data=resualt.json())
