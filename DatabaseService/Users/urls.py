ee8c86ed9411a639b47902060507daab98b51a1b
Mercedeh <Imercedeh@gmail.com>
2019-12-16 22:31:53 +0330

    Add get user url dbs

--- DatabaseService/Users/urls.py
+++ DatabaseService/Users/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import Signup
+from .views import GetUser
 from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
-    url(r'sign-up/',Signup.as_view()),
+    url(r'GetUser/',GetUser.as_view()),
     url(r'^token/$', jwt_views.TokenObtainPairView.as_view()),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
