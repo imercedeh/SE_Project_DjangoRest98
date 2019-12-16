ab8a8912fcc25e44ab9df79c2384dff0383f7c2e
Mercedeh <Imercedeh@gmail.com>
2019-12-16 20:03:19 +0330

    Add login url to us

--- UserService/Users/urls.py
+++ UserService/Users/urls.py
@@ -1,18 +1,16 @@
 from django.conf.urls import include, url
 from rest_framework.authtoken import views
-from Users.views import Signup
+from Users.views import Signup,Login
 from rest_framework import routers
 from rest_framework.authtoken import views
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework_simplejwt import views as jwt_views
 #router = routers.DefaultRouter()
 
 
 urlpatterns = [
     url(r'^sign-up/$', Signup.as_view()),
-    # url(r'^token/$', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # url(r'^token/refresh/$', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+     url(r'^token/$',Login.as_view()),
     # url(r'^leadercreation/$', LeaderCreationAPI.as_view()),
     # url(r'^me/$', ProfileAPI.as_view()),
     # url(r'SpecificLeader/',SpecificLeaderAPI.as_view()),
