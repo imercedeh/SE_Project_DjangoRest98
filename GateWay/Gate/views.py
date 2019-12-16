feceb22bca9127197e0b8347993d3f0ffaaca1b1
Mercedeh <Imercedeh@gmail.com>
2019-12-16 20:04:39 +0330

    Add login to gateway

--- GateWay/Gate/views.py
+++ GateWay/Gate/views.py
@@ -4,16 +4,24 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
 import requests
 from rest_framework.views import APIView
-
-UserServiceUrl="http://localhost:8001/api/User/"
+from GateWay.Global_variables import UserServiceURL
 
 class Signup(APIView):
     permission_classes = (AllowAny,)
     def post(self, request):
-        url=UserServiceUrl+"sign-up/"
+        url=UserServiceURL+"sign-up/"
         data=request.data
         resualt=requests.post(url=url,data=data)
         return Response(data=resualt.json())
+
+class Login(APIView):
+    permission_classes = (AllowAny,)
+    def post(self, request):
+        url=UserServiceURL+"token/"
+        data=request.data
+        resualt=requests.post(url=url,data=data)
+        return Response(data=resualt.json())
+
 
         
 
