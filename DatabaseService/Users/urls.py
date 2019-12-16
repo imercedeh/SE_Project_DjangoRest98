3bcb262d4e2d00ca8fe1792d5ece1498359e999f
Mercedeh <Imercedeh@gmail.com>
2019-12-16 23:50:37 +0330

    Add adduser url dbs

--- DatabaseService/Users/urls.py
+++ DatabaseService/Users/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import GetUser
+from .views import GetUser,AddUser
 from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     url(r'GetUser/',GetUser.as_view()),
+    url(r'AddUser/',AddUser.as_view()),
     url(r'^token/$', jwt_views.TokenObtainPairView.as_view()),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
