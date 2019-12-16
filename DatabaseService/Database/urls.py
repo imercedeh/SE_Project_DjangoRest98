70c5b12bcedabd8d090518da0c568f62fe59bfd6
Mercedeh <Imercedeh@gmail.com>
2019-12-16 18:36:24 +0330

    Remove new database service

--- DatabaseService/Database/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from django.contrib import admin
-
-from .views import UsersUsername
-
-urlpatterns = [
-    url(r'username/', UsersUsername.as_view(), name='get_username'), 
-]
