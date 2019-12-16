be84e41cbcec94caf2ef2f5eb59601329685dac6
Mercedeh <Imercedeh@gmail.com>
2019-12-16 22:31:24 +0330

    Edit global var dbs

--- DatabaseService/DatabaseService/Global_variables.py
+++ DatabaseService/DatabaseService/Global_variables.py
@@ -1,4 +1,4 @@
 UserServiceURL="http://127.0.0.1:8001/api/User/"
 PlaceServiceURL="http://127.0.0.1:8002/api/Place"
 TravellougeServiceURL="http://127.0.0.1:8003/api/Travellouge/"
-DatabaseServiceURL="http://127.0.0.1:8004/Query/"
+DatabaseServiceURL="http://127.0.0.1:8004"
