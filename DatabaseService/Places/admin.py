8c8659c8916bc34d73781da40276eb219c0a8d9e
Mercedeh <Imercedeh@gmail.com>
2019-12-16 15:52:35 +0330

    Add places to admin DBS

--- DatabaseService/Places/admin.py
+++ DatabaseService/Places/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from Places.models import Places
 # Register your models here.
+
+class PlaceModelAdmin(admin.ModelAdmin):
+    class Meta:
+        model=Places
+
+
+admin.site.register(Places, PlaceModelAdmin)
