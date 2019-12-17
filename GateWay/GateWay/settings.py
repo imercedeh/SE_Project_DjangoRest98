4c391911488788190f5bd63cd2ec5e4847e57a80
Mercedeh <Imercedeh@gmail.com>
2019-12-18 00:32:53 +0330

    Add authentication settings gateway

--- GateWay/GateWay/settings.py
+++ GateWay/GateWay/settings.py
@@ -41,6 +41,13 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+    
+}
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
