f0cf65dd1fb63db30a6d8b882eec892052a1c198
Mercedeh <Imercedeh@gmail.com>
2019-12-18 00:31:17 +0330

    Add leadercreation url gateway

--- GateWay/Gate/UserServiceURLs.py
+++ GateWay/Gate/UserServiceURLs.py
@@ -1,11 +1,11 @@
 from django.conf.urls import include, url
 from rest_framework.authtoken import views
-from Gate.views import Login,Signup
 
 from rest_framework import routers
 from rest_framework.authtoken import views
 from django.conf import settings
 from django.conf.urls.static import static
+from Gate.views import(Signup,Login,LeaderCreation)
 from rest_framework_simplejwt import views as jwt_views
 #router = routers.DefaultRouter()
 
@@ -13,8 +13,7 @@ from rest_framework_simplejwt import views as jwt_views
 urlpatterns = [
      url(r'^sign-up/$',Signup.as_view()),
      url(r'^token/$', Login.as_view()),
-    # url(r'^token/refresh/$', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-    # url(r'^leadercreation/$', LeaderCreationAPI.as_view()),
+     url(r'^leadercreation/$', LeaderCreation.as_view()),
     # url(r'^me/$', ProfileAPI.as_view()),
     # url(r'SpecificLeader/',SpecificLeaderAPI.as_view()),
     # url(r'SpecificUser/',SpecificUserAPI.as_view()),
