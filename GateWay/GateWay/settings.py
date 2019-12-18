2f7eb4c08443c11ac752c889b657f7d588ec9e27
Mercedeh <Imercedeh@gmail.com>
2019-12-18 23:59:07 +0330

    Remove extra settings

--- GateWay/GateWay/settings.py
+++ GateWay/GateWay/settings.py
@@ -43,9 +43,9 @@ INSTALLED_APPS = [
 ]
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    # 'DEFAULT_AUTHENTICATION_CLASSES': (
+    #     'rest_framework_simplejwt.authentication.JWTAuthentication',
+    # )
     
 }
 
--- UserService/UserService/settings.py
+++ UserService/UserService/settings.py
@@ -58,9 +58,9 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'UserService.urls'
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )   
+    # 'DEFAULT_AUTHENTICATION_CLASSES': (
+    #     'rest_framework_simplejwt.authentication.JWTAuthentication',
+    # )   
 
 }
 
