a61716778429cce2c23d38374575afedb6a58e25
Mercedeh <Imercedeh@gmail.com>
2019-12-19 00:00:50 +0330

    Add getleader url

--- DatabaseService/Users/urls.py
+++ DatabaseService/Users/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import GetUser,AddUser,AddLeader
+from .views import GetUser,AddUser,AddLeader,GetLeader
 from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     url(r'GetUser/',GetUser.as_view()),
     url(r'AddUser/',AddUser.as_view()),
+    url(r'GetLeader/',GetLeader.as_view()),
     url(r'AddLeader/',AddLeader.as_view()),
     url(r'^token/$', jwt_views.TokenObtainPairView.as_view()),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
