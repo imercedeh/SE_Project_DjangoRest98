d5979f5be8828fe83583a1e11907e1ca0c7ba977
Mobina <mobina.km@gmail.com>
2019-12-19 21:32:10 +0330

    Add urls in PlaceService.

--- PlaceService/Places/urls.py
+++ PlaceService/Places/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import url
 from django.contrib import admin
-
-from .views import CreatePlaceAPIView,ViewPlaceAPI,UniquePlaceAPI,RandomPlaces,PlaceAdvanceSearch,SearchView
+from django.conf import settings
+from django.conf.urls.static import static
+from Places.views import CreatePlace,UniquePlace#,ViewPlaceAPI,UniquePlaceAPI,RandomPlaces,PlaceAdvanceSearch,SearchView
 
 urlpatterns = [
-    url(r'CreatePlace/', CreatePlaceAPIView.as_view(), name='create'), 
-    url(r'ViewPlace/', ViewPlaceAPI.as_view(), name='View'), 
-    url(r'UniquePlace/',UniquePlaceAPI.as_view(),name='unique'),  
-    url(r'RandomPlace/',RandomPlaces.as_view(),name='random'),
-    url(r'PlaceAdvanceSearch/',PlaceAdvanceSearch.as_view({'get': 'list'}),name='PlaceAdvanceSearch'), 
-    url(r'SearchView/',SearchView.as_view(),name='serachview'),
-]
+    url(r'CreatePlace/', CreatePlace.as_view(), name='create'), 
+    # url(r'ViewPlace/', ViewPlaceAPI.as_view(), name='View'), 
+    url(r'UniquePlace/',UniquePlace.as_view(),name='unique'),  
+    # url(r'RandomPlace/',RandomPlaces.as_view(),name='random'),
+    # url(r'PlaceAdvanceSearch/',PlaceAdvanceSearch.as_view({'get': 'list'}),name='PlaceAdvanceSearch'), 
+    # url(r'SearchView/',SearchView.as_view(),name='serachview'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
