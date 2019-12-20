f4d8cfb60f4d0226aac38b4af6d902a1a88e0412
Mobina <mobina.km@gmail.com>
2019-12-20 15:18:55 +0330

    Add UniquePlace in Gateway Views.

--- GateWay/Gate/PlaceServiceURLs.py
+++ GateWay/Gate/PlaceServiceURLs.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
-from Gate.views import CreatePlace#,ViewPlace,UniquePlace,RandomPlaces,PlaceAdvanceSearch,SearchView
+from Gate.views import CreatePlace,UniquePlace#,ViewPlace,RandomPlaces,PlaceAdvanceSearch,SearchView
 
 urlpatterns = [
     url(r'CreatePlace/', CreatePlace.as_view(), name='create'), 
-    # url(r'ViewPlace/', ViewPlace.as_view(), name='View'), 
-    # url(r'UniquePlace/',UniquePlace.as_view(),name='unique'),  
+    #url(r'ViewPlace/', ViewPlace.as_view(), name='View'), 
+    url(r'UniquePlace/',UniquePlace.as_view(),name='unique'),  
     # url(r'RandomPlace/',RandomPlaces.as_view(),name='random'),
     # url(r'PlaceAdvanceSearch/',PlaceAdvanceSearch.as_view({'get': 'list'}),name='PlaceAdvanceSearch'), 
     # url(r'SearchView/',SearchView.as_view(),name='serachview'),
