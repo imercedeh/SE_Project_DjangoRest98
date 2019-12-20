9120a5125e50d64d5d18877e07a2f1033548730b
Mobina <mobina.km@gmail.com>
2019-12-20 15:17:46 +0330

    Edit DatabaseService PlaceSerializer.

--- DatabaseService/Places/serializers.py
+++ DatabaseService/Places/serializers.py
@@ -28,12 +28,55 @@ class CreatePlaceSerializer(ModelSerializer):
         read_only_fields = ('Average',)
 
 
+class SpecificSerializer(serializers.Serializer):
+    objID=serializers.IntegerField()
 
-# class ViewPlaceSerializer(ModelSerializer):
-    # leader=PlaceLeader(read_only=True,many=True)
-    # class Meta:
-        # model=Places
-        # fields='__all__' 
+
+class ViewPlaceSerializer(ModelSerializer):
+    #pp=uuSerializer(read_only=True)
+    #leader=PlaceLeader(read_only=True)
+    image1=SerializerMethodField()
+    class Meta:
+        model=Places
+        #fields='__all__' 
+        fields = [
+        'id',
+        'title', 
+        'image1',
+        'image2',
+        'image3',
+        'Description',
+        'Likes',
+        'categories',
+        'Hardness',
+        'Address',
+        'Time',
+        'StartTime',
+        'EndTime',
+        'City',
+        'Average',
+        ]
+
+    def get_image1(self, obj):
+        try:
+            image = "http://127.0.0.1:8000"+obj.image1.url
+        except:
+            image = None
+        return image
+
+    def get_image2(self, obj):
+        try:
+            image = "http://127.0.0.1:8000"+obj.image2.url
+        except:
+            image = None
+        return image
+
+    def get_image3(self, obj):
+        try:
+            image = "http://127.0.0.1:8000"+obj.image3.url
+        except:
+            image = None
+        return image
 
 
 # class HomePlaces(ModelSerializer):
