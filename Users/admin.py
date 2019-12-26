4c7899831406b24886fb8779b8740a24101e2577
Mercedeh <Imercedeh@gmail.com>
2019-12-27 00:36:15 +0330

    Add model to admin page

--- Users/admin.py
+++ Users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from Users.models import user,Leader
+from Users.models import user,Leader,LeaderRate
 
 admin.site.register(user)
 admin.site.register(Leader)
+admin.site.register(LeaderRate)
