34c9857958a506801e3e1557ba93b1fac6656fdd
Mobina <mobina.km@gmail.com>
2019-12-27 18:06:38 +0330

    Add Comment urls.

--- Places/api/urls.py
+++ Places/api/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from .views import CreatePlaceAPIView,ViewPlaceAPI,UniquePlaceAPI,RandomPlaces,PlaceAdvanceSearch,SearchView
+from .views import CreatePlaceAPIView,ViewPlaceAPI,UniquePlaceAPI,RandomPlaces,PlaceAdvanceSearch,SearchView,PostComment,GetComment
 
 urlpatterns = [
     url(r'CreatePlace/', CreatePlaceAPIView.as_view(), name='create'), 
@@ -10,4 +10,6 @@ urlpatterns = [
     url(r'RandomPlace/',RandomPlaces.as_view(),name='random'),
     url(r'PlaceAdvanceSearch/',PlaceAdvanceSearch.as_view({'get': 'list'}),name='PlaceAdvanceSearch'), 
     url(r'SearchView/',SearchView.as_view(),name='serachview'),
+    url(r'PostComment/',PostComment.as_view(),name='postcomment'),
+    url(r'GetComment/',GetComment.as_view(),name='getcomment'),    
 ]
