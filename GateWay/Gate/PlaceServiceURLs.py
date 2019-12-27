e4d398434795164fd98f70462d2a74236f5ffbdc
Mobina <mobina.km@gmail.com>
2019-12-27 18:22:36 +0330

    Add Random Place url in GateWay.

--- GateWay/Gate/PlaceServiceURLs.py
+++ GateWay/Gate/PlaceServiceURLs.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url
 from django.contrib import admin
-from Gate.views import CreatePlace,UniquePlace#,ViewPlace,RandomPlaces,PlaceAdvanceSearch,SearchView
+from Gate.views import CreatePlace,UniquePlace,RandomPlace#,ViewPlace,,PlaceAdvanceSearch,SearchView
 
 urlpatterns = [
     url(r'CreatePlace/', CreatePlace.as_view(), name='create'), 
     #url(r'ViewPlace/', ViewPlace.as_view(), name='View'), 
     url(r'UniquePlace/',UniquePlace.as_view(),name='unique'),  
-    # url(r'RandomPlace/',RandomPlaces.as_view(),name='random'),
+    url(r'RandomPlace/',RandomPlace.as_view(),name='random'),
     # url(r'PlaceAdvanceSearch/',PlaceAdvanceSearch.as_view({'get': 'list'}),name='PlaceAdvanceSearch'), 
     # url(r'SearchView/',SearchView.as_view(),name='serachview'),
 ]
