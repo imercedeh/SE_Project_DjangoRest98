48fb200b952f4796941057f354f17bb394c1fc43
Mobina <mobina.km@gmail.com>
2019-12-27 18:50:56 +0330

    Remove Useless Comments on PlaceViews.

--- Places/api/views.py
+++ Places/api/views.py
@@ -103,64 +103,30 @@ class UniquePlaceAPI(APIView):
         print(serializer)
 
         place=Places.objects.get(id=serializer.data['objID'])
-        #leader=Leader.objects.get(id=place.id)
-        #leader=Places.objects.get(place.leader)
         dataa=place.leader.all()
-        #leaders={}
         specificdatas={}
         specificdatas['leaders']=[]
         dic={}
-        print("hi")
         print(dataa)
         for i in dataa:
             u=user.objects.get(username=i.userID)
             serializer3=self.serializer_class3(u)
-            #d=serializer3.data
-            #print(type(d))
-            #specificdatas['leader']=serializer3.L
             print(i)
             dic['id']=serializer3.data['id']
             dic['avatar']=serializer3.data['avatar']
             dic['username']=serializer3.data['username']
             print(dic)
             specificdatas['leaders'].append(dict(dic))
-            # specificdatas.update()
-
-        print("--------------------")
-        print(specificdatas)
 
         serializer2=self.serializer_class2(place)
-        #serializer3=self.serializer_class3(u)
-
-        # specificdatas={}
-        # d=serializer3.data
-        # #print(type(d))
-        # specificdatas['avatar']=url+serializer3.data['avatar']
-        # specificdatas['username']=serializer3.data['username']
-        # specificdatas.(d['avatar'])
-        # specificdatas.append(d['username'])
         data=serializer2.data
-        data.update(specificdatas)#dict(specificdatas))
-        #data.update(leaders)
+        data.update(specificdatas)
         return Response(data,status=status.HTTP_200_OK)
 
-# class UniquePlaceAPI(generics.ListAPIView):
 
-    # queryset=Places.objects.all()
-    # serializer_class=ViewPlaceSerializer
-    # filter_backends= [SearchFilter]
-    # search_fields = ['id']
-    
-    # def get_queryset(self):
-        # queryset = Places.objects.all()
-        # id = self.request.query_params.get('search')
-        # if id is not None:
-             # queryset = queryset.filter(id__exact=id).distinct()
-        # return queryset
 
 
 class RandomPlaces(APIView):
-    #queryset = Places.objects.all().order_by('?')[:3]
     serializer_class = HomePlaces
 
     def get(self,request):
@@ -170,64 +136,23 @@ class RandomPlaces(APIView):
         specificdata['Home Place']=[]
         uniquenumbers=[]
 
-        #for third in range(0,3):
+
         while(len(uniquenumbers) < 3 ):
-            # uniquecount=len(uniquenumbers)
-            # print(uniquecount)
-            # if uniquecount < 4 :#or  uniquecount == 0:
             number=random.randint(1,len(data))
             print(number)
             if number not in uniquenumbers:
                 uniquenumbers.append(number)
-                # else:
-                #     break
 
-        print("---------------for balayy----")
-        print(uniquenumbers)
-
-        # for j in range(0,3):
-        #     print(j)
-        #     datacount=len(data)
-        #    # number=random.sample(range(1,datacount),1)
-        #     number=random.randint(1,datacount)
-        #     if number not in uniquenumbers and uniquenumbers.count==3:
-        #         uniquenumbers.append(number)
-        #     else:
-        #         return uniquenumbers
-
-            # # print("--------------------unique numbers------")
-            # # print(uniquenumbers)
-            # if(uniquenumbers.__contains__(number)):
-
-            # uniquenumbers.append(number)
-            # print("------------------number random---------------------------")
-            # print(number)
-            #for i in data:
         for k in uniquenumbers:
-                    # print("--------------i------")
-                    # print(i)
-                    # print("--------------k------")
-                    # print(k)
             placeid=data.get(id=k)
-                    # print("----------------place id------------")
-                    # print(placeid)
             serializer1=self.serializer_class(placeid)
 
             dic['id']=serializer1.data['id']
             dic['title']=serializer1.data['title']
             dic['image1']=serializer1.data['image1']
             dic['categories']=serializer1.data['categories']
-                # print("---------------dic-----")
-                # print(dic)
 
-        # for element in dic:
-        #     if element not in specificdata:
             specificdata['Home Place'].append(dict(dic))
-
-        # print("--------specific------------")
-        # print(specificdata)
-
-
         return Response(specificdata,status=status.HTTP_200_OK)
 
 
@@ -242,9 +167,7 @@ class PlaceAdvanceSearch(viewsets.ModelViewSet):
 
 
 class PostComment(APIView):
-    #queryset = Comment.objects.all()
     serializer_class=CommentSerializer
-    #permission_classes = (IsAuthenticated,)
 
     def post(self,request,format=None):
         serializer=self.serializer_class(data=request.data)
@@ -257,41 +180,10 @@ class PostComment(APIView):
             try:
                 comments=Comment(comment=comment,user=owner,place=place)
                 comments.save()
-                # print("------------comment-----")
-                # print(comments)
-
-                # print("---------placelist------------")
-
-                # print(placelist)
-                # for p in placelist:
-                #     place=Places.objects.get(id=p)
-                #     comments.place.add(p)
-                    # comments.place.add(place)
-
-                
-
-                # for p in placelist:
-                #     places=Places.objects.get(id=p)
-                #     print(places)
-                #     comments.place.add(places)
-                #     print("----------------comments after place----")
-                #     print(comments)
-
-                # print("-------------owner--------------------")
-                
-                # print(owner)
-                # comments.user.add(owner)
-
-                #comments.save()
-
-                # for p in place:
-                #     places=Places.objects.get(id=p)
-                #     comments.place.add(places)
-
-                content = {'detail': 'Place Added Successfully!!'}
+                content = {'detail': 'Comment Added Successfully!!'}
                 return Response(content,status=status.HTTP_201_CREATED)
             except:
-                content = {'detail': 'Failed To Add A Place!!'}
+                content = {'detail': 'Failed To Add A Comment!!'}
                 return Response(content,status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors,
@@ -315,28 +207,14 @@ class GetComment(APIView):
             dic={}
             serilizer2=self.serializer_class2(comment)
             dataa=Places.objects.get(id=serilizer2.data['place'])
-            print("------------------")
-            print(dataa)
-            print("-------------owner--------------------")
-                        # for i in dataa:
             serializer4=self.serializer_class4(dataa)
-            # dic['place']=(serializer4.data)
             owner=user.objects.get(id=serilizer2.data['user'])
             serializer3=self.serializer_class3(owner)   
             dic['Owner']=serializer3.data['username']
             dic['id']=serializer3.data['id']
             print(owner)
             specificdatas['Comment'].append(dict(dic))
-            #placelist=Places.objects.get(id=serilizer2.data['place'])
-            print("-------------plaaaaaaaaaaace--------------------")
-
-  
-            
-            print("hi")
-            
-
 
         data=serializer4.data
-        print("******************************data")
         data.update(specificdatas)
         return Response(data,status=status.HTTP_200_OK)
