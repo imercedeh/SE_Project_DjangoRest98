3e1802b0d364067b5b5270ce1675a9e2b442b485
Mercedeh <Imercedeh@gmail.com>
2020-01-26 02:15:44 +0330

    Add lead place url us

--- UserService/Users/urls.py
+++ UserService/Users/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from rest_framework.authtoken import views
-from Users.views import Signup,Login,LeaderCreation
+from Users.views import Signup,Login,LeaderCreation,LeadPlace
 from rest_framework import routers
 from rest_framework.authtoken import views
 from django.conf import settings
@@ -15,7 +15,7 @@ urlpatterns = [
     # url(r'^me/$', ProfileAPI.as_view()),
     # url(r'SpecificLeader/',SpecificLeaderAPI.as_view()),
     # url(r'SpecificUser/',SpecificUserAPI.as_view()),
-    # url(r'LeadPlace/',LeadPlaceAPI.as_view()),
+    url(r'LeadPlace/',LeadPlace.as_view()),
     # url(r'ChangeAvailability/',ChangeAvailabilityAPI.as_view()),
     # url('LeadersView/',LeadersView.as_view(),name='LeadersView'),
     # url('LeaderSortView/',LeaderSortView.as_view(),name='LeaderSortView'),
