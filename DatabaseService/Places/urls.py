c6b5834d14ea6b3c8ed4fffde5aec65cabe85b1d
Mercedeh <Imercedeh@gmail.com>
2020-01-26 02:11:01 +0330

    Add get place urls dbs

--- DatabaseService/Places/urls.py
+++ DatabaseService/Places/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import AddPlace,GetUniquePlace,GetRandomPlace#,AllPlace
+from .views import GetPlace,AddPlace,GetUniquePlace,GetRandomPlace#,AllPlace
 
 
 urlpatterns = [
+    url(r'GetPlace/',GetPlace.as_view()),
     url(r'AddPlace/',AddPlace.as_view()),
     url(r'GetUniquePlace/',GetUniquePlace.as_view()),
     url(r'GetRandomPlace/',GetRandomPlace.as_view()),
