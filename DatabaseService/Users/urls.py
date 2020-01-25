0b534c6a39c31c17dc7cbadaf6f1d544eccd4204
Mercedeh <Imercedeh@gmail.com>
2020-01-26 02:35:35 +0330

    Add get place url dbs

--- DatabaseService/Users/urls.py
+++ DatabaseService/Users/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import GetUser,AddUser,AddLeader,GetLeader,LeadPlace
+from .views import GetUser,AddUser,AddLeader,GetLeader,LeadPlace,GetPlaceLeader
 from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
@@ -10,5 +10,6 @@ urlpatterns = [
     url(r'GetLeader/',GetLeader.as_view()),
     url(r'AddLeader/',AddLeader.as_view()),
     url(r'LeadPlace/',LeadPlace.as_view()),
+    url(r'GetPlaceLeader/',GetPlaceLeader.as_view()),
     url(r'^token/$', jwt_views.TokenObtainPairView.as_view()),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
