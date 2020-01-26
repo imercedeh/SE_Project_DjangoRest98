27f8b29464893c38d3fc0814363b8cec148d0a24
Mobina <mobina.km@gmail.com>
2020-01-27 01:47:57 +0330

    Add Post Test for Signup.

--- Users/tests/test_views.py
+++ Users/tests/test_views.py
@@ -7,3 +7,13 @@ from Places.api.views import UniquePlaceAPI
 from rest_framework.test import APIRequestFactory
 from django_filters import filters
 from django_filters.rest_framework import DjangoFilterBackend, FilterSet 
+
+
+factory = APIRequestFactory()
+
+
+class ViewTestCase(TestCase):
+    def test_post_creation(self):
+        c = Client()  
+        response = c.post('http://127.0.0.1:8000/api/User/sign-up/',{'username': 'john','email':'john@gmail.com', 'password': 'smith','first_name':'johny','last_name' :'depp'})
+        self.assertEqual(response.status_code, 400)
